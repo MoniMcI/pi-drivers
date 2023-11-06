28a7b994fb212f132679e863415c433df62c2592
Zucca-dev <zucca.develop@gmail.com>
2023-11-06 17:06:01 -0300

    deploy1

--- server/src/db.js
+++ server/src/db.js
@@ -7,10 +7,10 @@ const TeamModel = require('./models/Team')
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/drivers`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require`, {
   logging: false, 
   native: false, 
 });
