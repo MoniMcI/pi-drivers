88f955c5e6a3d3556b1f4e23536c4737ae4260c0
Mónica Guantay <123341063+MoniMcI@users.noreply.github.com>
2025-12-05 17:49:39 -0300

    Change PORT to use environment variable or default

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 
-const PORT = 3001;
+const PORT = process.env || 3001;
 
 
 const { Driver, Team } = require('./src/db') // Asegúrate de importar tus modelos para crear el registro
 
 
-conn.sync({ force: true })
+conn.sync({ alter: true })
 .then( async () => {
 
 server.listen(PORT, () => {
